247fd1828cbc340bfe340ec472a3e60eb2f65cb4
utsav52 <utsavmaniyar23@gmail.com>
2022-04-26 11:39:35 +0530

    Practical 5

---
+++ Practical_05/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Practical_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write whatever you would like to:");
+            string text = Console.ReadLine();
+            File.WriteAllText("file.txt", text);
+            try
+            {
+                string entereddata = File.ReadAllText("file.txt");
+                Console.WriteLine(entereddata);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
